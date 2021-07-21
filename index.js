fce5758bfb7bda9b7cb926fac65a45c6c54a5f77
Michael Zidar <mzidar@gmail.com>
2021-07-21 10:19:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+let a;
+let multi = Math.round(a);
+function emptyFunction(){};
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multi){
+    return function fareMultiplier(fare){
+        return multi*fare;
+    }
+}
+
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, emptyFunction){
+    return emptyFunction(drivers);
+}
